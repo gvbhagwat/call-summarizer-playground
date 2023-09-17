0c05f994bb52cb77466e95240367197242eb2eba
Gaurav Bhagwat <bhagwat.gv@gmail.com>
2023-09-17 10:17:51 +0530

    Coming up with folder structure

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-import logging
 import uuid
 from fastapi import FastAPI, Request, HTTPException, Depends
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from loguru import logger
+from opentracing import Span, tag
 from app.api.endpoints import user, auth, transcription
-from app.db.session import SessionLocal
 from app.core.config import settings
-
-# Setup structured logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] [%(request_uuid)s] %(message)s")
-logger = logging.getLogger(__name__)
+from app.core.tracing import init_jaeger_tracer
 
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI(
@@ -31,27 +28,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Dependency to get logger with request UUID
-def get_logger(request: Request):
-    custom_logger = logging.LoggerAdapter(logger, {"request_uuid": request.state.uuid})
-    return custom_logger
+# Loguru configuration
+logger.add("logs/app.log", rotation="1 day", retention="10 days", level="INFO")
 
-# Include the API routers
-app.include_router(user.router, prefix="/users", tags=["users"])
-app.include_router(auth.router, prefix="/auth", tags=["authentication"])
-app.include_router(transcription.router, prefix="/transcription", tags=["transcription"])
-
-@app.on_event("startup")
-async def startup_event():
-    logger.info("Starting up the application...")
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    logger.info("Shutting down the application...")
-
-@app.get("/", tags=["root"])
-async def read_root():
-    return {"message": "Welcome to the Audio Summarizer API!"}
+# Initialize Jaeger tracer
+tracer = init_jaeger_tracer("audio-summarizer")
 
 # Middleware to assign a unique UUID to each request for tracking
 class RequestUUIDMiddleware(BaseHTTPMiddleware):
@@ -64,14 +45,32 @@ app.add_middleware(RequestUUIDMiddleware)
 
 # Middleware to log each request and response
 @app.middleware("http")
-async def log_requests(request: Request, call_next, logger: logging.LoggerAdapter = Depends(get_logger)):
-    logger.info(f"Path: {request.url.path} | Method: {request.method}")
+async def log_requests(request: Request, call_next):
+    logger.info(f"Request UUID: {request.state.uuid} | Path: {request.url.path} | Method: {request.method}")
     response = await call_next(request)
-    logger.info(f"Response Status: {response.status_code}")
+    logger.info(f"Request UUID: {request.state.uuid} | Response Status: {response.status_code}")
     return response
 
 # Global exception handler
 @app.exception_handler(HTTPException)
-async def custom_http_exception_handler(request: Request, exc: HTTPException, logger: logging.LoggerAdapter = Depends(get_logger)):
-    logger.error(f"Error: {exc.detail}")
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    logger.error(f"Request UUID: {request.state.uuid} | Error: {exc.detail}")
     return {"detail": exc.detail}
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Starting up the application...")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Shutting down the application...")
+    tracer.close()
+
+@app.get("/", tags=["root"])
+async def read_root():
+    return {"message": "Welcome to the Audio Summarizer API!"}
+
+# Include the API routers
+app.include_router(user.router, prefix="/users", tags=["users"])
+app.include_router(auth.router, prefix="/auth", tags=["authentication"])
+app.include_router(transcription.router, prefix="/transcription", tags=["transcription"])
